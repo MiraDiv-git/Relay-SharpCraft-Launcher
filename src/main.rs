0de3acfe30a3f9b03523158c1915eeb5459a29b1
MiraDiv-git <omega24122004@gmail.com>
2026-03-17 05:33:08 +0200

    Localization in build Added localization to output binary

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,29 @@
 use slint::ComponentHandle;
 use std::collections::HashMap;
-use std::fs;
 use std::sync::{Arc, Mutex};
 
 slint::include_modules!();
 
 fn main() {
     let ui = MainWindow::new().unwrap();
+    let default_lang = "en";
 
-    let translations = Arc::new(Mutex::new(HashMap::<String, String>::new()));
+    let mut locales = HashMap::new();
+    locales.insert("ua", include_str!("../lang/ua.json"));
+    locales.insert("en", include_str!("../lang/en.json"));
 
-    // Связываем колбэк Slint с нашим HashMap в Rust
+    let initial_json = locales.get(default_lang).unwrap_or(&"{}");
+    let current_map: HashMap<String, String> = serde_json::from_str(initial_json).unwrap_or_default();
+
+    let translations = Arc::new(Mutex::new(current_map));
+
     let tr_clone = translations.clone();
     ui.global::<L10n>().on_get(move |key| {
         let map = tr_clone.lock().unwrap();
         map.get(key.as_str())
-            .map(|s| s.as_str().into())
+            .map(|s| s.to_string().into())
             .unwrap_or(key)
     });
 
-    load_locale_to_map(&translations, "en");
-
     ui.run().unwrap();
-}
-
-fn load_locale_to_map(map_arc: &Arc<Mutex<HashMap<String, String>>>, lang: &str) {
-    let path = format!("lang/{}.json", lang);
-    if let Ok(content) = fs::read_to_string(path) {
-        if let Ok(json) = serde_json::from_str::<HashMap<String, String>>(&content) {
-            let mut map = map_arc.lock().unwrap();
-            *map = json;
-        }
-    }
 }
